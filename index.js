f30d3c183e7efbe317b8cab70a98f533f1424bc2
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-02-18 18:43:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 function theBeatlesPlay(musicians, instruments){
   var result = [];
   for (var i = 0; i < musicians.length; i++) {
-    result.push('${musicians[i]} plays ${instruments[i]}');
+    result.push(`${musicians[i]} plays ${instruments[i]}`);
   }
   return result;
 }
 
 function johnLennonFacts(facts){
-  var i = 0;
-  while (i < facts.length) {
-    facts[i] += "!!!";
-    i++;
+  for (let i = 0; facts.length > i; i++){
+  facts[i] += "!!!"
   }
   return facts;
 }
